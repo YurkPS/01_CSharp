1a2aee20cef61d189717bb4a656a61b5ebe58f3a
YurkPS <yurkovps@yandex.ru>
2023-11-15 22:37:19 +0300

    add task 4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5;
+int b = 6;
+int c = 25;
+int d = 7;
+int e = 6;
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.Write("Maximum is: ");
+Console.Write(max);
