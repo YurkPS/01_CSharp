d8edb5f6547d844e5bbe8c48d45510a220b8a186
YurkPS <yurkovps@yandex.ru>
2023-11-15 22:46:20 +0300

    add task 5

---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 100;
+int i = 1;
+
+//while (i <= n)
+//{
+//    Console.Write(i);
+//    i = i + 1;
+//}
+
+while (i <= n)
+{
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
