2d5f46383f5c1e007c797454cb7dc7fecb970fbc
YurkPS <yurkovps@yandex.ru>
2023-11-18 23:50:14 +0300

    add lesson2-task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = new int[5];
+
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 3;
+arr[3] = 8;
+arr[4] = 1;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+Console.WriteLine(arr[5]);//ERROR
+
+int [] arr2 = new int[5] {1,2,3,4,5}; // Полный формат создания и заполнения
+int [] arr3 = {1,2,3,4,5}; //Упрощенный формат
+//array = [1,2,3,4,5]; //Массив Python
