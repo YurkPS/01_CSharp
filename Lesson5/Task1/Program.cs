0ed3ab1393da6c18dba200942b16f941d9248469
YurkPS <yurkovps@yandex.ru>
2023-12-10 10:02:23 +0300

    add Lesson5/Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Рекурсия. Задача по открытию матрешки
+
+//Условия, когда рекурсивный вызов стоит после
+// void OpenMatryoshka(int size)
+// {
+//     Console.WriteLine($"Opening matryoshka of size {size}...");
+//     // Базовый случай: самая маленькая матрёшка
+//     if (size == 1)
+//     {
+//         Console.WriteLine("Reached the smallest matryoshka!");
+//         return;
+//     }
+//     OpenMatryoshka(size - 1);
+// }
+
+// OpenMatryoshka(5);
+
+// Условие, когда рекурсивный вызов стоит перед - результат от минимальной к максимальной
+
+void OpenMatryoshka(int size)
+{
+    if (size == 1)
+    {
+       Console.WriteLine("Smallest!");
+       return;
+    }
+    OpenMatryoshka(size - 1);
+    Console.WriteLine($"Opening matryoshka of size {size}...");
+}
+
+OpenMatryoshka(5);
